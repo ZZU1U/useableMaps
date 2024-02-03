69a044449ef5fbb9da1431e6a857b1cb8e586d02
PLidiia <151846713+PLidiia@users.noreply.github.com>
2024-02-03 16:07:19 +0700

    Заготовка API - часть. 1 задания static-maps

--- usablemaps/api/maps.py
+++ usablemaps/api/maps.py
@@ -1,8 +1,20 @@
-cord_x = None
-cord_y = None
-
-scale = 1
-
-def get_picture(cord_x, cord_y, scale):
+dates_coords = input().split(', ')
+dates_move = input().split(', ')
+params = {
+    'll': ','.join(dates_coords),
+    'spn': ','.join(dates_move),
+    'l': 'sat'
+}
+api_server = "http://static-maps.yandex.ru/1.x/"
+response = requests.get(api_server, params=params)
+if response:
     pass
-
+else:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+map_file_1 = "map.png"
+with open(map_file_1, "wb") as file:
+    file.write(response.content)
+os.remove(map_file_1)
