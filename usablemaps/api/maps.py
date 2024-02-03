7e7a7594230034d8713089edc8e9358851885f21
PLidiia <151846713+PLidiia@users.noreply.github.com>
2024-02-03 16:30:21 +0700

    Доработка Заготовка API - часть. 1 задания static-maps

--- usablemaps/api/maps.py
+++ usablemaps/api/maps.py
@@ -1,20 +1,29 @@
-dates_coords = input().split(', ')
-dates_move = input().split(', ')
-params = {
-    'll': ','.join(dates_coords),
-    'spn': ','.join(dates_move),
-    'l': 'sat'
-}
-api_server = "http://static-maps.yandex.ru/1.x/"
-response = requests.get(api_server, params=params)
-if response:
-    pass
-else:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-map_file_1 = "map.png"
-with open(map_file_1, "wb") as file:
-    file.write(response.content)
-os.remove(map_file_1)
+import requests
+
+
+def map_zoom(dates_coords, dates_move):
+    dates = [dates_move, dates_move]
+    params = {
+        'll': ','.join(dates_coords),
+        'spn': ','.join(dates),
+        'l': 'sat'
+    }
+    response = requests.get(api_server, params=params)
+    # elif PgDown убавляем
+    dates = [dates_move - zoom_map / 111, dates_move]
+    params = {
+        'll': ','.join(dates_coords),
+        'spn': ','.join(dates),
+        'l': 'sat'
+    }
+    response = requests.get(api_server, params=params)
+    if response:
+        pass
+    else:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+    map_file_1 = "map.png"
+    with open(map_file_1, "wb") as file:
+        file.write(response.content)
