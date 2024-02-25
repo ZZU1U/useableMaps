6d85075990658b4e7f621cd585937e98a109c8cc
ZZU1U <zzu1u@list.ru>
2024-02-25 17:07:17 +0700

    First task (After Review)

--- usablemaps/main.py
+++ usablemaps/main.py
@@ -1,27 +1,10 @@
-import tempfile
 import sys
 from PyQt5.QtWidgets import QApplication
 from gui.app import MyWidget
-from api.maps import get_map
 
-cords = list(map(int, input('Введите координаты через пробел: ').split()))
 
-scale = float(input('Введите масштаб:'))
-
-app = QApplication(sys.argv)
-window = MyWidget()
-
-with tempfile.NamedTemporaryFile() as file:
-    cont = get_map(cords, scale)
-
-    if cont:
-        file.write(cont)
-    else:
-        print('Произошла ошибка!')
-        sys.exit(1)
-
-    window.ChangeMapImage(file.name)
-
-window.show()
-
-sys.exit(app.exec_())
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyWidget()
+    window.show()
+    sys.exit(app.exec_())
